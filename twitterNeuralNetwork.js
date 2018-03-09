cd12cd7881c5efb730c020dd3017e51a469e0133
tracycollins <tc@threeceemedia.com>
2018-03-09 00:54:26 -0500

    remove histogramParser

--- twitterNeuralNetwork.js
+++ twitterNeuralNetwork.js
@@ -61,7 +61,7 @@ const wordAssoDb = require("@threeceelabs/mongoose-twitter");
 const userServer = require("@threeceelabs/user-server-controller");
 const User = mongoose.model("User", wordAssoDb.UserSchema);
 
-const histogramParser = require("@threeceelabs/histogram-parser");
+// const histogramParser = require("@threeceelabs/histogram-parser");
 
 require("isomorphic-fetch");
 // const Dropbox = require("dropbox").Dropbox;
